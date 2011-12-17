18c1279967b339a5c22b43df05a934092cadec11
Kacper Bielecki <kazjote@ragnarson.com>
2011-12-17 18:19:31 +0100

    Cleaned up upload code

--- server.js
+++ server.js
@@ -1,31 +1,17 @@
 var
   console = require('console'),
   http = require('http'),
-  fs = require('fs'),
   path = require('path'),
-  formidable = require('formidable'),
-  static = require('./lib/static')
+  static = require('./lib/static'),
+  upload = require('./lib/upload'),
   port = 8080
 
 http.createServer(function(request, response) {
+  var rootPath = path.resolve('.')
   if(request.method == 'GET') {
-    static.handleRequest(request, response, path.resolve('.'))
+    static.handleRequest(request, response, rootPath)
   } else {
-    var form = new formidable.IncomingForm()
-    form.uploadDir = './upload'
-    var uploadedFile = null
-
-    form.on('file', function(field, file) {
-      uploadedFile = file
-    })
-
-    form.on('end', function() {
-      response.writeHead(200, {'Content-Type': 'text/plain'})
-      response.write(uploadedFile.path)
-      response.end()
-    })
-
-    form.parse(request)
+    upload.handleRequest(request, response, rootPath)
   }
 }).listen(8080)
 
