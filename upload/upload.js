6926af39f54599f56698fd301b64abd5d27f5327
kazjote <kazjote@ragnarson.com>
2011-12-18 22:39:40 +0100

    Fixes for ie7

--- upload/upload.js
+++ upload/upload.js
@@ -4,8 +4,9 @@ $(document).ready(function() {
       key  = Math.floor(Math.random() * 100000000),
       form = $(this).attr('action', '/' + key),
       updateStatus = function() {
+        var random = Math.floor(Math.random() * 100000000)
         $.ajax({
-          url: '/status/' + key,
+          url: '/status/' + key + '?ie_random=' + random,
           success: function(progress) {
             var parsedProgress = Math.ceil(parseFloat(progress) * 100)
             $('#upload_progress').text(parsedProgress + '%')
@@ -22,6 +23,7 @@ $(document).ready(function() {
 
     $(this).ajaxSubmit({
       success: function(filename) {
+        filename = filename.replace('<PRE>', '').replace('</PRE>', '')
         var filepath = 'http://' + location.host + '/upload/' + filename
         $('#submit_button').removeAttr('disabled')
         $('#path').show()
